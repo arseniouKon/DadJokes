d261237ef2b0f84d1d15bb35f4e930dfc488b1ad
Konstantinos Arseniou <arsenioukon97@gmail.com>
2023-10-18 16:53:07 +0300

    Initial commit.

---
+++ DadJokes/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DadJokes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
