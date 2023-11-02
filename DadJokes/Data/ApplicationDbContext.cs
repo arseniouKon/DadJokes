07e40289c83694ec40fd30653fd70f2a6b9806c1
Konstantinos Arseniou <arsenioukon97@gmail.com>
2023-11-03 00:54:24 +0200

    Added FirstName, LastName and Nickname to the registration form and changed the Hello-message with the user's nickname upon login. Also made minor changes.

--- DadJokes/Data/ApplicationDbContext.cs
+++ DadJokes/Data/ApplicationDbContext.cs
@@ -4,12 +4,18 @@ using DadJokes.Models;
 
 namespace DadJokes.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
         public DbSet<DadJokes.Models.Joke> Joke { get; set; } = default!;
     }
 }
